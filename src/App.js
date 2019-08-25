40b47cd70e2dfbd3b531ce1c55c0d16f652df099
Ivan Derkach <horovik88@gmail.com>
2019-08-25 17:54:50 +0300

    update 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './App.css';
-import {Post} from "./post/Post.jsx";
+import { Post } from "./components/Post/Post.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Post/>
+      <Post />
     </div>
   );
 }
