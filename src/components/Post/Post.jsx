7ccceddd35a0679d8ed72e0e24cfff9d1b51c4c2
Ivan Derkach <horovik88@gmail.com>
2019-08-26 15:32:56 +0300

    update 2

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import "./Post.css"
 
-export function Post() {
-  return (
-    <div>hello</div>
-  )
+export class Post extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      postText: "",
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://bloggy-api.herokuapp.com/posts")
+      .then((response) => {
+      return response.json()
+    }).then((loadedPosts) => {
+      this.setState({posts: loadedPosts})
+    });
+  }
+
+  // updatePostText = postText => {
+  //   this.setState({
+  //     postText: postText
+  //   });
+  // };
+
+
+  render() {
+    return (
+      <div className='Post'>
+
+        <div className='Post__title'>Title</div>
+        {this.posts && this.posts.map((post, i) => {
+          return <div className="Post__text">`${[i].body}`</div>
+        })}
+
+        <div className='Post__footer'>
+          <button className='updateBtn' id="updateBtn">Update</button>
+
+          <button>Comment</button>
+          <button>Delete</button>
+        </div>
+
+      </div>
+    )
+  }
 }
---
+++ src/components/UpdateModal/UpdateModal.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./UpdateModal.css";
+
+export function f() {
+
+
+
+  return (
+    <div className='update__modal'>
+      <div className='update__modal_content'>
+        <input className='modal__input' type="text"/>
+      </div>
+    </div>
+  )
+}
